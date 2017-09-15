194cf20d6430b56a62dde196a4dbd4430f853979
Suzanne Wu <wu.wu.wu.world@gmail.com>
2017-09-15 19:44:16 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,11 +1,6 @@
 var name = "Susan"
 var height = "70"
 
-if (name === "Susan") {
-  console.log ('The name is correct.')
-} else {
-  console.log ('Expected name to be Susan')
-}
 
 // Don't worry about this
 module.exports = { name, height
